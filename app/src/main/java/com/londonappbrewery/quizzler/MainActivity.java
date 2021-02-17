74552fb54de88cd42268e66322842b48c255000b
harshit <jainharshit218@gmail.com>
2021-02-17 13:59:01 +0530

    changinf

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -41,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
     int index=0;
     int score=0;
 
-    // TODO: Uncomment to create question bank
+
+
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
             new TrueFalse(R.string.question_1, true),
             new TrueFalse(R.string.question_2, true),
